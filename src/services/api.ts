b12be5fa604e608da4d7313413092871be1e3c6c
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-04-11 17:35:47 -0300

    chore: Update vercel url env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://${
-    process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost'
-  }:3000/api`,
+  baseURL: `http://${process.env.VERCEL_URL || 'localhost'}:3000/api`,
 });
 
 export { api };
